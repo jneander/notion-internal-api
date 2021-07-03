69262663bdd07d8a9b2e6c468c4e5fb6b0d8f7b3
Jeremy Neander <jeremy.neander@gmail.com>
2021-07-03 10:27:11 -0500

    feat: update relation type with collection pointer

--- src/notion/properties/relation.ts
+++ src/notion/properties/relation.ts
@@ -1,13 +1,23 @@
 import {Uuid} from '../shared'
 import {PropertyKey} from './keys'
 
+type CollectionPointer = {
+  id: Uuid
+  spaceId: Uuid
+  table: 'collection'
+}
+
 export type RelationProperty = {
   collection_id: Uuid
+  collection_pointer?: CollectionPointer
   name: string
   property: PropertyKey
   type: 'relation'
 }
 
-type RelationPropertyValueItem = ['‣', [['p', Uuid /* pageId */, Uuid? /* spaceId */]]]
+type RelationPropertyValueItem = [
+  '‣',
+  [['p', Uuid /* pageId */, Uuid? /* spaceId */]]
+]
 
 export type RelationPropertyValue = Array<RelationPropertyValueItem | [',']>
